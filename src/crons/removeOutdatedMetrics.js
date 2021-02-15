f485b587f2669444683b88799b0b06b9774655dd
Husseiny <husseiny.mohammed@gmail.com>
2021-02-15 19:41:19 +0200

    removed console log

--- src/crons/removeOutdatedMetrics.js
+++ src/crons/removeOutdatedMetrics.js
@@ -5,7 +5,6 @@ const dataStore = require('../data/DataStore');
 
 exports.removeOutdatedMetrics = () => {
   cron.schedule(CronExpressionOutdatedMetrics, () => {
-    console.log('remove outdated metrics');
     dataStore.removeOutdatedMetrics();
   });
 };
